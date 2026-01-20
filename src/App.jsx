b77b10c90c7cdc329f73c5b9f7e62d06cb4e1bd5
Chinmayee <chinmayeesr2@gmail.com>
2026-01-20 15:12:41 +0530

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 import Home from "./pages/Home";
 import Careers from "./pages/Careers";
 
