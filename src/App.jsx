e4a8831618fda27c1f007f38dbcf6a94b23b3888
Chinmayee <chinmayeesr2@gmail.com>
2026-01-23 19:09:45 +0530

    Describe what you changed in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,13 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Careers from "./pages/Careers";
 
-
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" element={<About />} />         {/* 👈 Default route */}
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/content" element={<Contact />} />
         <Route path="/careers" element={<Careers />} />
       </Routes>
     </>
