3967512c5701a35001f5b0eacee61dcc1b52897e
Chinmayee <chinmayeesr2@gmail.com>
2026-01-28 18:26:05 +0530

    Fix  error

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,27 +1,40 @@
 import { Link, useLocation } from "react-router-dom";
 import "../Style/Navbar.css";
-<li>
-  <Link to="/" className={location.pathname === "/" ? "active" : ""}>
-    About us
-  </Link>
-</li>
-
 
 export default function Navbar() {
   const location = useLocation();
 
   return (
-    <nav className="navbar">
+    <header className="navbar">
       <div className="logo">Ozta Labs</div>
-      <ul className="nav-links">
-  <li><Link to="/" className={location.pathname === "/" ? "active" : ""}>About us</Link></li>
-  <li><Link to="/home" className={location.pathname === "/home" ? "active" : ""}>Home</Link></li>
-  <li><Link to="/contact" className={location.pathname === "/contact" ? "active" : ""}>Contact</Link></li>
-  <li><Link to="/careers" className={location.pathname === "/careers" ? "active" : ""}>Careers</Link></li>
-     </ul>
-    </nav>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/about" className={location.pathname === "/about" ? "active" : ""}>
+              About us
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className={location.pathname === "/" ? "active" : ""}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" className={location.pathname === "/contact" ? "active" : ""}>
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link to="/careers" className={location.pathname === "/careers" ? "active" : ""}>
+              Careers
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
+
 
 
 
